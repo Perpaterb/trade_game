66678827c703374366b39eb530198a4f1097a2bf
Perpaterb <zcarss@gmail.com>
2021-03-15 16:57:17 +1100

    fixed the trading confirm tile and added more comments

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -2,15 +2,17 @@ class Holding < ApplicationRecord
     
     #combine all a users holdings of the same stock and asking but not AUD
     def self.combinesamesame(users_id)
-        comine = []
+        combine = []
+        #Quary Holding table in DB loop over
         Holding.all.each do |holdings|
             if holdings[:owner_users_ID] == users_id
                 if holdings[:stock_code] != "AUD"
+                    #Quary Holding table in DB loop over On^2 :(
                     Holding.all.each do |holdings2|
                         if holdings2[:id] != holdings[:id]
                             if holdings[:stock_code] == holdings2[:stock_code]
                                 if holdings[:asking] == holdings2[:asking]
-                                    comine << holdings
+                                    combine << holdings
                                 end
                             end
                         end
@@ -18,22 +20,26 @@ class Holding < ApplicationRecord
                 end                                
             end
         end
-        if comine.length > 0
-            comine = comine.uniq
+        if combine.length > 0
+            combine = combine.uniq
             total = 0
-            comine.each do |c|
-                total += c[:quantity]
-                Holding.destroy(c[:id])
+            combine.each do |com|
+                total += com[:quantity]
+                #Destroy all Holdings items in the DB with the same owner and stock and asking 
+                Holding.destroy(com[:id])
             end
-            Holding.create!(owner_users_ID: comine.first[:owner_users_ID], stock_code: comine.first[:stock_code], quantity: total, asking: comine.first[:asking])
+            #Create Holding item in the DB for the combined stock.
+            Holding.create!(owner_users_ID: combine.first[:owner_users_ID], stock_code: combine.first[:stock_code], quantity: total, asking: combine.first[:asking])
         end
     end
     
     #remove all holdings with 0 quantity but not AUD
     def self.removezerons(users_id)
+        #Quary Holding table in DB loop over
         Holding.all.each do |holdings|
             if holdings[:stock_code] != "AUD"
                 if holdings[:quantity] <= 0
+                    #Destroy all Holding items that have 0 quantity and are not AUD
                     Holding.destroy(holdings[:id])
                 end
             end
@@ -44,10 +50,13 @@ class Holding < ApplicationRecord
     def self.splitholding(holding_changes, stock_id, stock_code, users_id)
         if holding_changes[:asking].to_f < -5 or holding_changes[:asking].to_f > 5
             return "Error: Asking need to be between -5 and +5"
+            #Quary Holding table in DB for itme quantity
         elsif holding_changes[:quantity].to_i > Holding.find(stock_id)[:quantity]
             return "Error: Quantity exceeds available quantity"
         else
+            #Update Holding itme in DB with remaining quantity
             Holding.where(:id => stock_id).update_all(:quantity => (Holding.find(stock_id)[:quantity].to_i - holding_changes[:quantity].to_i))
+            #Create Holding item in the DB with new asking and quantity
             Holding.create!(owner_users_ID: users_id, stock_code: stock_code, quantity: holding_changes[:quantity].to_i, asking: holding_changes[:asking].to_f * 10 .to_i)
             return "Holdings updated"
         end
@@ -55,6 +64,7 @@ class Holding < ApplicationRecord
 
     #send the users avalible funds in AUD 
     def self.useravaliblefunds(users_id)
+        #Quary Holding table in DB loop over
         Holding.all.each do |holdings|
             if holdings[:owner_users_ID] == users_id
                 if holdings[:stock_code] == "AUD"
@@ -67,8 +77,10 @@ class Holding < ApplicationRecord
     # finalise a trade
     def self.tradeconfirmed(trade_id , purchasing_user_id, quantity)
         quantity = quantity.to_f
+        #Quary Holding table in DB for itme with the trade ID
         trade = Holding.find(trade_id)
         @selling_user_id = trade[:owner_users_ID]
+        #Quary Holding table in DB loop over all
         Holding.all.each do |holdings|
             if holdings[:owner_users_ID] == purchasing_user_id
                 if holdings[:stock_code] == "AUD"
@@ -87,11 +99,16 @@ class Holding < ApplicationRecord
         final_cost = (((realtime_stock_value * quantity) * 100).round)/100
         if @purchasing_user_funds > final_cost
             if trade[:quantity] >= quantity
+                #Update item in Holding table in DB purchasing_user - AUD
                 Holding.where(:id => @purchasing_user_funds_id).update_all(:quantity => @purchasing_user_funds - final_cost)
+                #Update item in Holding table in DB selling_user + AUD
                 Holding.where(:id => @selling_user_funds_id).update_all(:quantity => @selling_user_funds + final_cost)
+                #Update item in Holding table in DB selling_user - quantity
                 Holding.where(:id => trade_id).update_all(:quantity => trade[:quantity] - quantity)
+                #create item in Holding table in DB purchasing_user stock and quantity
                 Holding.create!(owner_users_ID: purchasing_user_id, stock_code: trade[:stock_code], quantity: quantity, asking: 50)
                 
+                #create item in Transaction table in DB with all the info of the trade
                 Transaction.create!(sold_user_id: @selling_user_id, buying_user_id: purchasing_user_id, stock_code: trade[:stock_code], quantity: quantity, price_per_share: realtime_stock_value)
 
                 return "Trade in compleated at a final cost of $#{final_cost}"
@@ -126,6 +143,7 @@ class Holding < ApplicationRecord
     #if Historical Stock Price is less then 30 min old then just get the Historical Stock Price.
     def self.getlivestockprice(stockcode)
         @price_in_history = 0
+        #Quary HistoricalStockPrice table in DB loop over all to see in price info on the requested sock in less then 30 min old 
         HistoricalStockPrice.all.reverse().each do |stock|
             if @price_in_history == 0
                 if stock[:stockcode] == stockcode
@@ -138,6 +156,7 @@ class Holding < ApplicationRecord
             end
         end 
         if @price_in_history == 0
+            #if price info on the requested sock in more then 30 min old scrap curect price from morningstar.com.au
             p "!!!! getting price from morningstar.com.au"
             url = "https://www.morningstar.com.au/Stocks/NewsAndQuotes/" + stockcode.to_s
             require 'nokogiri'
@@ -148,6 +167,7 @@ class Holding < ApplicationRecord
                 docdata << link.content
             end
             @price = docdata[18].to_f
+            #Create item in HistoricalStockPrice table in DB with updated price.
             updatehistoricalstockprice(stockcode, @price)
         end
         p "!!!! price is #{@price}"
@@ -156,6 +176,7 @@ class Holding < ApplicationRecord
 
     #update the Historical Stock Price
     def self.updatehistoricalstockprice(stockcode, price)
+        #Create item in HistoricalStockPrice table in DB with updated price.
         HistoricalStockPrice.create!(stockcode: stockcode, price: price)
     end
 
--- app/models/start_cart_item.rb
+++ app/models/start_cart_item.rb
@@ -4,15 +4,18 @@ class StartCartItem < ApplicationRecord
     def self.addtocart(user_id, quantity, stock_code)
         skip = 0
         if quantity > 0
+            #Quary StartCartItem table in DB loop over each
             StartCartItem.all.each do |item|
                 if item[:owner_users_id] == user_id
                     if item[:stock_code] == stock_code
+                        #Update StartCartItem table in DB with the added quantity
                         StartCartItem.where(:id => item[:id]).update_all(:quantity => item[:quantity].to_i + quantity)
                         skip = 1
                     end
                 end
             end
             if skip == 0
+                #Create new item is the StartCartItem table in DB with the added quantity
                 StartCartItem.create!(owner_users_id: user_id, stock_code: stock_code, quantity: quantity)
             end
         end 
@@ -20,6 +23,7 @@ class StartCartItem < ApplicationRecord
 
     #remove an item from the cart
     def self.removefromcart(cart_id)
+        #Destroy StartCartItem item in the DB
         StartCartItem.destroy(cart_id)
     end
 
@@ -27,12 +31,16 @@ class StartCartItem < ApplicationRecord
     def self.addstockstoholding(user_id)
         total = getcarttotal(user_id)
         if total <= 10001
+            #Quary StartCartItem table in DB loop over each
             StartCartItem.all.each do |item|
                 if item[:owner_users_id] == user_id
+                    #Create new item is the Holding table in DB for each of the items is the users shopping cart. #Should realy removefromcart(cart_id) after this but might want for records.
                     Holding.create!(owner_users_ID: user_id, stock_code: item[:stock_code], quantity: item[:quantity], asking: 50)
                 end
             end
+            #Create new item is the Holding table in DB for 10,000 AUD
             Holding.create!(owner_users_ID: user_id, stock_code: "AUD", quantity: 10000, asking: 0)
+            #Update User itme in user table in DB to say that that they have finished the setup
             User.where(:id => user_id).update_all(:signinstep => 2 ) 
         end
     end
@@ -40,9 +48,11 @@ class StartCartItem < ApplicationRecord
     #get the total cost of the cart
     def self.getcarttotal(user_id)
         @total = 0
+        #Quary StartCartItem table in DB loop over each to get the total chart value of a user.
         StartCartItem.all.each do |item|
             p "!!!! owner id =#{item[:owner_users_id]}   --   user_id = #{user_id}"
             if item[:owner_users_id] == user_id
+                # Run getlivestockprice on holding model
                 @total = @total + (item[:quantity] * Holding.getlivestockprice(item[:stock_code]))
                 p "!!!!! add in #{item[:quantity]} of #{Holding.getlivestockprice(item[:stock_code])} -- to cart"
             end
